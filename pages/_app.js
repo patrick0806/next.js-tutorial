97211604feced810418cfe94aaa365e778a35b26
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2021-03-02 08:43:49 -0300

    blog tutorial complete

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+//É neste arquivo que os importes de estilo global devem ser importados
+import '../styles/global.css'
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
